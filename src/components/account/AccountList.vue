fa60e9a3c8870d0669cb926755e595a9bc50503b
Jordi Mellema <Xen0nEv_e@outlook.com>
2021-12-29 22:04:09 +0100

    Installed and configured Axios, enabling communication between the wallet and the node. Can now receive a list of transactions of a corresponding wallet from the node.

--- src/components/account/AccountList.vue
+++ src/components/account/AccountList.vue
@@ -1,36 +1,48 @@
 <template>
-    <div class="accordion" role="tablist">
-        <b-card no-body class="mb-1" v-for="wallet in wallets" :key="wallet.seedphrase">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle="'accordion' + wallet.address" variant="primary">{{ wallet.name }} (100 EC)</b-button>
-            </b-card-header>
-            <b-collapse :id="'accordion' + wallet.address" visible accordion="my-accordion" role="tabpanel">
-                <b-card-body>
-                    <h4>Transactions</h4>
-                    <b-table striped hover :items="items"></b-table>
-                </b-card-body>
-            </b-collapse>
-        </b-card>
-    </div>
+  <div class="accordion" role="tablist">
+    <b-card no-body class="mb-1" v-for="wallet in wallets" :key="wallet.seedphrase">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle="'accordion' + wallet.address" variant="primary" v-on:click="getItems(wallet.address)">
+          {{ wallet.name }} (100 EC)
+        </b-button>
+      </b-card-header>
+      <b-collapse :id="'accordion' + wallet.address" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <h4>Transactions</h4>
+          <b-table striped hover :items="items"></b-table>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import {mapState} from "vuex";
+import axios from "axios";
 
 @Component({
-    computed: {
-        ...mapState(['wallets'])
-    }
+  computed: {
+    ...mapState(['wallets'])
+  }
 })
 export default class AccountList extends Vue {
-    public items = [
-        { from: '0xFF', to: '0xAA', amount: 40, timestamp: '15-10-2021 14:32:33' },
-        { from: '0xFF', to: '0xAA', amount: 40, timestamp: '15-10-2021 14:32:33' },
-        { from: '0xFF', to: '0xAA', amount: 40, timestamp: '15-10-2021 14:32:33' },
-        { from: '0xFF', to: '0xAA', amount: 40, timestamp: '15-10-2021 14:32:33' },
-        { from: '0xFF', to: '0xAA', amount: 40, timestamp: '15-10-2021 14:32:33' },
-    ];
+  public items = [];
+
+  public getItems(address : string) {
+    axios.get(`http://localhost:4567/transactions/` + address)
+        .then(response => {
+          const data = response.data.data;
+          this.items = [];
+
+          data.forEach(obj => {
+            this.items.push({ from: obj.from, to: obj.to, amount: obj.amount, timestamp: obj.timestamp.iMillis });
+          });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+  }
 
 }
 </script>
